dd0a6a11cfa74039c8c63471c4585601241e7ba0
alternativshik <alternativshik@gmail.com>
2013-01-14 10:52:07 +0200

    #2 add setup file and MANIFEST

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='django-multifilefield',
+    version='0.9',
+    description='Django model and form fields for storring html5 forms multiupload data',
+    author='Sergey Maltsev',
+    author_email='alternativshik@gmail.com',
+    url='https://github.com/Cyberic/django-multifilefield',
+    packages=[
+        'multifilefield',
+    ]
+)
